3379b3e719ea886d17a2a227607b3db21ca7036f
febin <febinsunny2004@gmail.com>
2025-07-07 21:50:01 +0530

    Completed

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list,todo_detail
 from . import views
 
 urlpatterns = [
 
-     path('todos/', views.todo_list),
+     path('todos/', views.todo_list,name="todo_list"),
+     path('todos/<int:pk>/', views.todo_detail,name="todo_detail"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -13,13 +13,38 @@ from .models import Todo
 def todo_list(request):
     if request.method == 'GET':
         todos=Todo.objects.all()
+        print(todos)
         serializer=TodoSerializer(todos,many=True)
         return Response(serializer.data)
     elif request.method=='POST':
-        serializer=TodoSerializer(data=serializer.data)
+        serializer=TodoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    
+@api_view(['GET','PUT','DELETE'])
+
+def todo_detail(request,pk):
+   try:
+      todo=Todo.objects.get(pk=pk)
+   except Exception as e:
+      return Response ({'error':'Not found'},status=status.HTTP_404_NOT_FOUND)
+   
+   if request.method=='GET':
+       serializers=TodoSerializer(todo)
+       return Response(serializers.data)
+   elif request.method=='PUT':
+       serializers=TodoSerializer(todo,data=request.data)
+       if serializers.is_valid():
+           serializers.save()
+           return Response(serializers.data)
+       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+   elif request.method=='DELETE':
+       todo.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
+       
+        
+
     
