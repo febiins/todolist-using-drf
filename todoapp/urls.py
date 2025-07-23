379f8df4c2a2fddca5ebabf23233bd3e3288210f
febin <febinsunny2004@gmail.com>
2025-07-23 08:05:46 +0530

    token is added

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import todo_list,todo_detail
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
 
      path('todos/', views.todo_list,name="todo_list"),
      path('todos/<int:pk>/', views.todo_detail,name="todo_detail"),
+     path('api-token-auth/', obtain_auth_token),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from.serializers import TodoSerializer
@@ -9,6 +10,7 @@ from .models import Todo
 # Create your views here.
 
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 
 def todo_list(request):
     if request.method == 'GET':
@@ -25,6 +27,7 @@ def todo_list(request):
     
     
 @api_view(['GET','PUT','DELETE'])
+@permission_classes([IsAuthenticated])
 
 def todo_detail(request,pk):
    try:
